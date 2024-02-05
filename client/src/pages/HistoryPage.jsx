6be690be29182b180647120817565fc45561b6c8
Juan Manuel <arenasrinconj@gmail.com>
2024-02-05 16:18:43 -0500

    el historial de actividades para cada usuario funciona, solo falta estilarlo #7

---
+++ client/src/pages/HistoryPage.jsx
@@ -0,0 +1,45 @@
+import { getAllActivities } from "../api/activity"
+import { useState, useEffect } from "react"
+import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
+
+function History() {
+    const [ activities, setActivities ] = useState([])
+
+    const getActivities = async () => {
+        try {
+            const res = await getAllActivities(window.localStorage.getItem('token'))
+            console.log(res)
+            setActivities(res.data)
+        }catch(err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getActivities()
+    },[])
+
+    return (
+        <div className="flex flex-col justify-between items-center">
+            <NavBar />
+                <main className={`relative md:w-3/4 bg-white p-6 my-10 `}>
+                    {
+                        activities && activities.map(activity => (
+                            <li key={activity._id}>
+                                <p>{activity.methodology}</p>
+                                <p>{activity.topic}</p>
+                                <p>{activity.competence}</p>
+                                <p>{activity.subject}</p>
+                                <p>{activity.tools}</p>
+                                <p>{activity.generatedClass}</p>
+                            </li>
+                        ))
+                    }
+                </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default History
