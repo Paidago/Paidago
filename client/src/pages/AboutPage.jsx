f60a283c3017933a843d14a65c285923ca5bd44d
Hamilton <hamilt727@gmail.com>
2024-02-11 20:36:51 -0500

    se agrega contenido a la pagina #3

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -4,21 +4,20 @@ import Footer from "../components/Footer"
 function About(){
 
     return (
-      <div className="flex flex-col justify-between">
+      <div className="flex flex-col min-h-screen justify-between">
         <NavBar />
         <div id="content" className="mt-7">
-          <div id="primary" className="content-area">
+          <div id="primary" className="content-area flex-grow">
             <main className="w-full flex items-center justify-center">
               <article className="md:w-3/4 bg-slate-100 p-6 mb-10">
-                <h1 className="text-black text-5xl font-bold mx-auto py-5">
+                <h1 className="text-black text-5xl font-bold text-center py-5">
                   ¿QUIÉNES SOMOS?
                 </h1>
-                <div className="py-10">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Soluta possimus dolor quasi itaque aliquam officiis obcaecati,
-                  non similique quis repellendus. Repudiandae soluta aliquid,
-                  velit pariatur blanditiis inventore necessitatibus atque
-                  vitae?
+                <div className="py-10 ">
+                  <p className="text-justify">
+                  En la pretensión de una educación renovada, y constante en el tiempo, la asociación paidago busca reivindicar los procesos formativos hacia lo realmente humano; recontruyendo lo establecido para arar el camino de la creatividad y la genialidad. Para esto compartimos ideas fundamentales de la pedagogía a través del asesoramiento, acompañamiento virtual, encuentros académicos y la producción visual.
+                  </p>
+                
                 </div>
               </article>
             </main>
