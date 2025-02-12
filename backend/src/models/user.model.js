544b45559c1bdeaf10d75d01769a7fb0ca8a0c83
juanma <arenasrinconj@gmail.com>
2025-02-11 19:53:28 -0500

    la pagina del perfil quedó mejor

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -17,6 +17,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    profilePicture: {
+        type: String,
+    },
     paymentID: {
         type: String
     }
